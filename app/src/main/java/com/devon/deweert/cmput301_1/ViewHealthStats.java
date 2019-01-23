d5b547adc7b6838c9d535a9f5a2036a6d8e59a64
Deweertd <deweertd1830@gmail.com>
2019-01-23 11:22:19 -0700

    View Health Stats page is working, edit button has no function yet

--- app/src/main/java/com/devon/deweert/cmput301_1/ViewHealthStats.java
+++ app/src/main/java/com/devon/deweert/cmput301_1/ViewHealthStats.java
@@ -1,13 +1,58 @@
 package com.devon.deweert.cmput301_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 public class ViewHealthStats extends AppCompatActivity {
+
+    private Intent intent;
+    private MyHealthStats myHealthStats;
+    private int position;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_health_stats);
+        intent  = getIntent();
+        getAllData();
+
+        setTextFields();
+    }
+
+    private void getAllData(){
+        Bundle dataBundle = intent.getExtras();
+        if(dataBundle != null){
+            breakupData(dataBundle);
+        }
+
+    }
+
+    private void breakupData(Bundle dataBundle){
+        String healthStatsToView = dataBundle.getString(MyHealthStats.HEALTH_DATA);
+        position = dataBundle.getInt(MyHealthStats.INT_DATA);
+        Type type = new TypeToken<MyHealthStats>() {}.getType();
+        myHealthStats = new Gson().fromJson(healthStatsToView, type);
+    }
+
+    private void setTextFields(){
+        String dateMade = myHealthStats.getDateMeasured() + " " + myHealthStats.getTimeMeasured();
+        int heartRate = myHealthStats.getHeartRate();
+        int diastolic = myHealthStats.getDiastolicPressure();
+        int systolic = myHealthStats.getSystolicPressure();
+        String comment = myHealthStats.getComment();
+        ((TextView)findViewById(R.id.DisplayDateMeasured)).setText(dateMade);
+        ((TextView)findViewById(R.id.EditHeartRateTextField)).setText(String.valueOf(heartRate));
+        ((TextView)findViewById(R.id.EditDiastolicPressureTextField)).setText(String.valueOf(diastolic));
+        ((TextView)findViewById(R.id.EditSystolicPressureTextField)).setText(String.valueOf(systolic));
+        ((TextView)findViewById(R.id.EditCommentTextField)).setText(comment);
     }
 }
